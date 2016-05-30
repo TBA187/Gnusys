07267ef08920ab69b000bd2daf7b8e2f1bf8e73e
s <s@DESKTOP-2PQ738K>
2016-05-30 11:58:17 +0200

    backup

--- Gnusys/Gnusys/Controllers/ReadingsController.cs
+++ Gnusys/Gnusys/Controllers/ReadingsController.cs
@@ -22,6 +22,10 @@ namespace Gnusys.Controllers
         [HttpPost]
         public ActionResult AddReadings(int OxygenSaturation_input, int Pulse_input)
         {
+            Readings r = new Readings() { Pulse = Pulse_input, OxygenSaturation = OxygenSaturation_input, Date = DateTime.Now };
+            
+            DB.Readings.Add(r);
+            DB.SaveChanges();
             return View();
         }
     }
