54d00c2ebf9c3252932273b0fb7d44917773e340
Sadri TBA <sadri_albaner@hotmail.com>
2016-05-30 12:41:29 +0200

    ny

---
+++ Gnusys/Gnusys/Models/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gnusys.Models
+{
+    public class Test
+    {
+        [Required(ErrorMessage = "Name skal udfyldes!")]
+        public string Name { get; set; }
+    }
+}
