bfa655ae24346ad00de98963e84771599d2178e0
s <s@DESKTOP-2PQ738K>
2016-05-30 13:33:17 +0200

    detvirkerigen !!!! plz dont jinx it

--- Gnusys/Gnusys/Controllers/ReadingsController.cs
+++ Gnusys/Gnusys/Controllers/ReadingsController.cs
@@ -1,12 +1,15 @@
 ﻿using Gnusys.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Gnusys.Controllers
 {
     public class ReadingsController : Controller
     {
-        GnysusEFModel DB = new GnysusEFModel();
+        GnusysEFModel DB = new GnusysEFModel();
         // GET: Readings
         public ActionResult Index()
         {
@@ -19,9 +22,6 @@ namespace Gnusys.Controllers
         [HttpPost]
         public ActionResult AddReadings(int OxygenSaturation_input, int Pulse_input)
         {
-            Readings r = new Readings() { Pulse = Pulse_input, OxygenSaturation = OxygenSaturation_input, Date = DateTime.Now };
-            DB.Readings.Add(r);
-            DB.SaveChanges();
             return View();
         }
     }
