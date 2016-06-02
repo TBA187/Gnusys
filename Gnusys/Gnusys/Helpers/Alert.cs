b2eb88feae50553501ab61af5ec52fb974cd149c
s <s@DESKTOP-2PQ738K>
2016-06-02 16:04:22 +0200

    lolz

---
+++ Gnusys/Gnusys/Helpers/Alert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gnusys.Helpers
+{
+    public static class Alert
+    {
+
+        public static void SendAlert()
+        {
+            //Do stuff to send some sort of alert
+        }
+    }
+}
