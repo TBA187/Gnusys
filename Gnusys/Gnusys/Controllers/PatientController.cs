e468f3790d0174e8a5e8d33057112426834ee4a2
Tina Blum Rasmussen <tina8369@edu.eal.dk>
2016-06-03 09:13:38 +0200

    Oprettet patientsView + patient controller - stort set tomt endnu. Tilføjet dropdown/devices i addPatient

---
+++ Gnusys/Gnusys/Controllers/PatientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gnusys.Models;
+
+namespace Gnusys.Controllers
+{
+    public class PatientController : Controller
+    {
+        GnysusEFModel DB = new GnysusEFModel();
+
+        // GET: Patient
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Patients()
+        {
+            return View();
+        }
+    }
+}
