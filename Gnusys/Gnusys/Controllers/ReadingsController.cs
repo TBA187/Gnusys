d92c7f03a6d57afa59458a9720c93d3a2ceed684
Ahmad Masseh Yar <ahma1659@edu.eal.dk>
2016-06-07 12:23:24 +0200

    69

--- Gnusys/Gnusys/Controllers/ReadingsController.cs
+++ Gnusys/Gnusys/Controllers/ReadingsController.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,15 +27,13 @@ namespace Gnusys.Controllers
         [HttpPost]
         public ActionResult AddReadings(int OxygenSaturation_input, int Pulse_input)
         {
-            int userid = int.Parse(Session["ID"].ToString());
+            int userid = int.Parse(Session["User"].ToString());
             Device d = DB.Device.FirstOrDefault(p => p.PatientID == userid);
             DeviceLine dl = new DeviceLine() { PatientID = userid, DeviceID = d.ID };
             Readings r = new Readings() { Pulse = Pulse_input, OxygenSaturation = OxygenSaturation_input, Date = DateTime.Now};
             r.DeviceLine.Add(dl);            
             DB.Readings.Add(r);
             DB.SaveChanges();
-            Helpers.AdvProg adv = new Helpers.AdvProg();
-            adv.CheckForSuddenDropOrRise(r, userid);
             return View();
         }
     }
